41b4d613c2d4f973ee46be45292e90351e5d2bf0
manuel-aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2023-04-13 19:20:30 +0100

    Create get_next_line_bonus.h

---
+++ get_next_line_bonus.h
@@ -0,0 +1,21 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define BUFFER_SIZE 42
+# define MAX_FD 1024
+# define BUFCLEAN MAX_FD + 1
+# define ENDFILE -1
+
+/* get_next_line.c*/
+char	*get_next_line(int fd);
+
+/* get_next_line_utils.c*/
+char	*gnl_strncat(char *buf, char *line, int *old_size, int inc_size);
+void	clean_buf(char *buf, int pivot);
+int	update_line(char *buf, char **line, int *line_len);
+
+#endif
